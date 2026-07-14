51073cdf966de71e097ba9a21ad5117da1b7f6d4
suliatr-alt <suliatr@gmail.com>
2026-07-15 00:00:32 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+window.addEventListener("scroll", function () {
+
+    const header = document.querySelector("header");
+
+    if (window.scrollY > 40) {
+        header.style.background = "#071b31";
+        header.style.boxShadow = "0 8px 30px rgba(0,0,0,.25)";
+    } else {
+        header.style.background = "#0B2744";
+        header.style.boxShadow = "none";
+    }
+
+});
